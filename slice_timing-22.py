095406a51e165146a89a1307cff9c85d3b39c7e7
Matthew Brett <matthew.brett@gmail.com>
2016-04-13 23:58:56 -0800

    Update documentation

--- slice_timing-22.py
+++ slice_timing-22.py
@@ -2,8 +2,13 @@
 
 plt.plot(times_slice_0, interped_vals, 'r:+',
     label='interpolated slice 1 time course')
+# [...]
 plt.plot(times_slice_0, time_course_slice_0, 'b:+',
     label='slice 0 time course')
+# [...]
 plt.legend()
+# <...>
 plt.title('Slice 1 time course interpolated to slice 0 times')
+# <...>
 plt.xlabel('time (seconds)')  # doctest: +SKIP
+# <...>
