113fc2f6363877017ca3e3ad3eb1225e0f092e33
Matthew Brett <matthew.brett@gmail.com>
2016-11-05 12:25:04 -0800

    Update documentation

--- on_cdfs-4.py
+++ on_cdfs-4.py
@@ -1,23 +1,31 @@
-# Show relationship of PDF to CDF for three threshold values.
-thresholds = (-1.5, 0, 1.5)
+# Show relationship of PDF and CDF for three example t values.
+example_values = (-1.5, 0, 1.5)
 pdf_values = t_dist.pdf(t_values)
 cdf_values = t_dist.cdf(t_values)
 fill_color = (0, 0, 0, 0.1)
 line_color = (0, 0, 0, 0.5)
-fig, ax = plt.subplots(2, len(thresholds), figsize=(10, 6))
-for i, t in enumerate(thresholds):
-    ax[0, i].plot(t_values, cdf_values)
-    ax[1, i].plot(t_values, pdf_values)
-    # Fill area at and to the left of threshold t.
-    ax[1, i].fill_between(t_values, pdf_values,
-                          where=t_values <= t,
-                          color=fill_color)
-    p = t_dist.pdf(t)  # Probability density at this threshold.
-    # Line showing position of t on x-axis of PDF plot.
-    ax[1, i].plot([t, t],
-                  [0, p], color=line_color)
-    c = t_dist.cdf(t)  # Cumulative distribution at this threshold.
-    # Lines showing t and CDF value for t on CDF plot.
-    ax[0, i].plot([t, t, ax[0, i].axis()[0]],
-                  [0, c, c], color=line_color)
-    ax[0, i].set_title('t = {:.1f}, area = {:.2f}'.format(t, c))
+fig, axes = plt.subplots(2, len(example_values), figsize=(10, 6))
+for i, x in enumerate(example_values):
+    cdf_ax, pdf_ax = axes[:, i]
+    cdf_ax.plot(t_values, cdf_values)
+    pdf_ax.plot(t_values, pdf_values)
+    # Fill area at and to the left of x.
+    pdf_ax.fill_between(t_values, pdf_values,
+                        where=t_values <= x,
+                        color=fill_color)
+    pd = t_dist.pdf(x)  # Probability density at this value.
+    # Line showing position of x on x-axis of PDF plot.
+    pdf_ax.plot([x, x],
+                [0, pd], color=line_color)
+    cd = t_dist.cdf(x)  # Cumulative distribution value for this x.
+    # Lines showing x and CDF value for t on CDF plot.
+    x_ax_min = cdf_ax.axis()[0]  # x position of y axis on plot.
+    cdf_ax.plot([x, x, x_ax_min],
+                [0, cd, cd], color=line_color)
+    cdf_ax.set_title('x = {:.1f}, area = {:.2f}'.format(x, cd))
+    # Hide top and right axis lines and ticks to reduce clutter.
+    for ax in (cdf_ax, pdf_ax):
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
